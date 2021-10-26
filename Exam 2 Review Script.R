94758dacd02874a9404cf5aa60969d14ec4918e9
annacalvertjackson <annacalvertjackson@gmail.com>
2021-10-26 10:06:25 -0500

    If_else statements

--- Exam 2 Review Script.R
+++ Exam 2 Review Script.R
@@ -16,3 +16,39 @@
 setwd("C:/Users/annac/Desktop/Grad School/Classes/Fall2021_Classes/Intro to R")
 
 getwd()
+
+##if_else statements
+
+best.candy<-c("reeses.pcups")
+
+halloween.time<-c("yes")
+
+if(best.candy=="mms") {
+  reaction<-"meh"
+} else if(halloween.time=="yes") {
+  reaction<-"hazzah!"
+}
+
+reaction
+
+##try this a different way
+
+best.candy<-c("reeses.pcups")
+
+halloween.time<-c("yes")
+
+pumpkin.spice<-c("good")
+
+if(best.candy=="mms") {
+  reaction<-"meh"
+} else if(halloween.time=="no") {
+  reaction<-"hazzah"
+} else if (pumpkin.spice=="good") {
+  reaction<-"delicious"
+}
+
+##R checks each "if" statement for something true and then "reacts"
+##skips over all false statements until it finds a true one
+
+reaction
+
