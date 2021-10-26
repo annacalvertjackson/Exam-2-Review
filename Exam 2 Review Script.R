dd94df731cdbe1088ef92954809f376fe84618ba
annacalvertjackson <annacalvertjackson@gmail.com>
2021-10-26 10:24:17 -0500

    wide to long practice

--- Exam 2 Review Script.R
+++ Exam 2 Review Script.R
@@ -1,5 +1,9 @@
 ##Review for Exam Oct. 26th
 
+setwd("C:/Users/annac/Desktop/Grad School/Classes/Fall2021_Classes/Intro to R")
+
+getwd()
+
 ##Version Control----
 
 ##Create a new repository in GitHub
@@ -10,14 +14,12 @@
 ##Make scripts as needed
 ##Make sure project and script(s) are "staged"
 ##Pull (blue arrow) first (?) to sync R studio with GitHub
-##Push changes to GitHub (green arrow)
 ##Commit to comment on version/changes
+##Push changes to GitHub (green arrow)
 
-setwd("C:/Users/annac/Desktop/Grad School/Classes/Fall2021_Classes/Intro to R")
 
-getwd()
 
-##if_else statements
+##if_else statements----
 
 best.candy<-c("reeses.pcups")
 
@@ -52,3 +54,48 @@ if(best.candy=="mms") {
 
 reaction
 
+##Wide to long----
+
+##use gather()
+
+library(tidyverse)
+
+auto<-mtcars
+
+head(auto)
+
+auto.long<-gather(data=auto, key="metrics", value="value", c(1:3))
+
+head(auto.long)                  
+
+unique(auto.long$metrics) 
+
+##so we gathered the first three columns into one single column "metrics"
+
+##can also use melt() from reshape2 package
+
+library(reshape2)
+
+auto.long.melt<-melt(data=auto, id.vars = c("hp", "drat", "wt", "qsec", "vs", "gear", "carb"),
+                     measure.vars = c("mpg", "cyl", "disp"),
+                     variable.name = "metrics")
+auto.long.melt
+
+
+##practice: use gather() on "hp" "wt" and "gear"
+
+practice<-gather(data=auto, key="metrics", value="value", c("hp", "wt", "gear") )
+
+practice
+
+##repeat exercise using melt()
+
+practice.melt<-melt(data=auto, id.vars=c("mpg", "cyl", "disp", "drat", "qsec", "vs", "carb", "am"), 
+                    measure.vars=c("hp", "wt", "gear"),
+                    variable.name = "metrics")
+practice.melt
+
+
+##id.vars are all the columns you want to keep the same
+##measure.vars are all the columns you want to melt into the same column
+##variable.name is what you want to call the new column (that your melted)
