b3d905a0d3bdd0f4589eaf984a3fcd3ddd3ae5b0
annacalvertjackson <annacalvertjackson@gmail.com>
2021-10-26 09:52:04 -0500

    Notes on how to work with Git/Version Control

---
+++ Exam 2 Review Script.R
@@ -0,0 +1,16 @@
+##Review for Exam Oct. 26th
+
+##Version Control----
+
+##Create a new repository in GitHub
+##Copy code
+##New project in R
+##Version Control/Git
+##Paste code
+##Make scripts as needed
+##Make sure project and script(s) are "staged"
+##Pull (blue arrow) first (?) to sync R studio with GitHub
+##Push changes to GitHub (green arrow)
+##Commit to comment on version/changes
+
+
